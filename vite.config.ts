cab09ba315f498e01942c0978d19d412554b5337
dorakemon <51844896+dorakemon@users.noreply.github.com>
2025-04-14 18:15:39 +0900

    feat: add deploy stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/jwp-debugger/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
